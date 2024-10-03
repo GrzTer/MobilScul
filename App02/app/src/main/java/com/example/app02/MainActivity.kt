b725b5ae72470ef331d11e35246eec6bd313c3c5
GrzegorzT <grzegorz.tereszkiewicz@infotech.edu.pl>
2024-10-03 16:06:58 +0200

    Zad05

--- App02/app/src/main/java/com/example/app02/MainActivity.kt
+++ App02/app/src/main/java/com/example/app02/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.app02
 
 import android.os.Bundle
+import android.provider.MediaStore.Audio.Radio
 import android.view.LayoutInflater
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.app02.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,8 +23,58 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        with(binding){
+//        with(binding) {
+//            button.setOnClickListener {
+//                val name = name.text
+//                val surname = surname.text
+//                val plec = radioG.checkedRadioButtonId
+//                val checkb = checkBox.text
+//                if (name.isNotEmpty()) {
+//                    label.text = name
+//                }
+//                if (surname.isNotEmpty()) {
+//                    label2.text = surname
+//                }
+//                if (plec.isNotEmpty()) {
+//                    label4.text = plec
+//                }
+//                if (checkb.isNotEmpty()) {
+//                    label5.text = checkb
+//                }
+//            }
+//        }
+//    }
+//}
+        with(binding) {
+            button.setOnClickListener {
+                val name = name.text.toString()
+                val surname = surname.text.toString()
+                val checkb = checkBox.text.toString()
 
+
+                val selectedRadioId = radioG.checkedRadioButtonId
+                if (selectedRadioId != -1) {
+                    val selectedRadioButton: RadioButton = findViewById(selectedRadioId)
+                    val plec = selectedRadioButton.text.toString()
+
+                    if (plec.isNotEmpty()) {
+                        label4.text = plec
+                    }
+                } else {
+                    Toast.makeText(this@MainActivity, "Please select a gender", Toast.LENGTH_SHORT).show()
+                }
+
+                // Set text fields to labels if not empty
+                if (name.isNotEmpty()) {
+                    label.text = name
+                }
+                if (surname.isNotEmpty()) {
+                    label2.text = surname
+                }
+                if (checkb.isNotEmpty()) {
+                    label5.text = checkb
+                }
+            }
         }
     }
 }
