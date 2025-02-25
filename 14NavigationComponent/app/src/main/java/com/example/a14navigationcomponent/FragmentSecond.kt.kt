cf934f08c3452260fff415657e4972013ae794d6
GrzegorzT <144318154+GrzTer@users.noreply.github.com>
2025-02-25 14:33:22 +0100

    .,.,.

---
+++ 14NavigationComponent/app/src/main/java/com/example/a14navigationcomponent/FragmentSecond.kt.kt
@@ -0,0 +1,28 @@
+package com.example.a14navigationcomponent
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.a14navigationcomponent.databinding.FragmentFirstBinding
+
+class `FragmentSecond.kt`: Fragment() {
+
+    var binding : FragmentFirstBinding? = null
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding!!.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        binding = null
+    }
+}
---
+++ 14NavigationComponent/app/src/main/java/com/example/a14navigationcomponent/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.a14navigationcomponent
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.a14navigationcomponent.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        val button = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        setContentView(button.root)
+
+    }
+}
