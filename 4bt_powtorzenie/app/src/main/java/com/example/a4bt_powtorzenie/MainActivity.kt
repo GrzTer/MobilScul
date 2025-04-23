ff7d3bc925d3deaddba7b09cb4131439f1c6ac8d
GrzegorzT <144318154+GrzTer@users.noreply.github.com>
2025-04-23 11:41:34 +0200

    .

---
+++ 4bt_powtorzenie/app/src/main/java/com/example/a4bt_powtorzenie/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.a4bt_powtorzenie
+
+import android.R
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.ArrayAdapter
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.a4bt_powtorzenie.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
+
+
+        var listaZadan = arrayListOf<String>()
+        var arrayAdapter = ArrayAdapter(this, R.layout.simple_list_item_1,listaZadan)
+        with (binding) {
+            listView.adapter = arrayAdapter
+            button.setOnClickListener {
+                listaZadan.add(editTextText.text.toString())
+//                arrayAdapter.notifyDataSetChanged()
+                    listView.adapter = arrayAdapter
+
+            }
+        }
+
+    }
+}
