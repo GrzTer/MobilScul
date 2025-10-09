3ab9d85f4c229c37bb0f42d500ee7379c4e0aaa4
GrzegorzT <144318154+GrzTer@users.noreply.github.com>
2025-10-09 15:12:17 +0200

    Update 0910 lesson

---
+++ 01_Wstep_do_jezyka_Kotlin/app/src/main/java/com/example/a01_wstep_do_jezyka_kotlin/konsola2.kt
@@ -0,0 +1,23 @@
+package com.example.a01_wstep_do_jezyka_kotlin
+
+
+fun main() {
+//    val liczba = 8
+//    if (liczba > 10) {
+//        println("Większa niż 10")
+//    } else if (liczba == 10) {
+//        println("Równa 10")
+//    } else {
+//        println("Mniejsza niż 10")
+//    }
+
+//    for (i in 1 downTo 5) {
+//        println("Liczba: $i")
+//    }
+
+//    var x = 0
+//    do {
+//        println("x: $x")
+//        x++
+//    } while (x < 3)
+}
