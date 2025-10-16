ea124b68e88aec39e8144bb4cd2f6b0b0a7d1b66
GrzegorzT <144318154+GrzTer@users.noreply.github.com>
2025-10-16 15:43:20 +0200

    Zadanie03

---
+++ 01_Wstep_do_jezyka_Kotlin/app/src/main/java/com/example/a01_wstep_do_jezyka_kotlin/konsola3.kt
@@ -0,0 +1,35 @@
+package com.example.a01_wstep_do_jezyka_kotlin
+
+fun main(){
+//    val owoce = listOf<String>("Jabłko", "Śliwka", "Gruszka")
+//    println(owoce.size)
+//    for (owoc in owoce) print(owoc)
+//    val matematyka = listOf(5,3,1,4)
+//     if(6 in matematyka ){ println("Gratulacje") }
+//    else{ println("Ucz się") }
+
+//    val listaStudentow = ArrayList<String>()
+//    listaStudentow.add("Ewa")
+//    listaStudentow.add("Jan")
+//    listaStudentow.add("Tomasz")
+//    println(listaStudentow)
+//    listaStudentow.remove("Jan")
+//    listaStudentow[0] = "Agnieszka"
+//    println(listaStudentow)
+//    listaStudentow[0] = "Jan"
+//    println(listaStudentow)
+//    val listaLiczb = ArrayList<Int>()
+//    for (i in 1..5) listaLiczb.add(i)
+//    println(listaLiczb)
+//    listaLiczb.remove(5)
+//    println(listaLiczb)
+//    listaLiczb.add(0,10)
+//    println(listaLiczb)
+
+    val liczby = arrayListOf<Int>()
+    liczby.add(11)
+    liczby.remove(7)
+    print(liczby)
+
+
+}
