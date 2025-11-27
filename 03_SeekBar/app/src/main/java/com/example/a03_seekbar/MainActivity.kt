467f88f4252580d28b9e8958a5e27aa1ebedd8c0
GrzegorzT <144318154+GrzTer@users.noreply.github.com>
2025-11-27 15:54:01 +0100

    End of 03 task

--- 03_SeekBar/app/src/main/java/com/example/a03_seekbar/MainActivity.kt
+++ 03_SeekBar/app/src/main/java/com/example/a03_seekbar/MainActivity.kt
@@ -2,7 +2,11 @@ package com.example.a03_seekbar
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.widget.CheckBox
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import android.widget.SeekBar
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -31,7 +35,34 @@ class MainActivity : AppCompatActivity() {
                 override fun onStopTrackingTouch(seekBar: SeekBar?) {
 
                 }
+            })
+            seekBar2.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+                override fun onProgressChanged(
+                    seekBar: SeekBar?,
+                    progress: Int,
+                    fromUser: Boolean
+                ) {
+                    textView2.text = "Jasność: "+progress.toString()+"%"
+                }
 
+                override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+                }
+
+                override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+                }
+            })
+            checkBox.setOnClickListener { if (checkBox.isChecked){
+                Toast.makeText(this@MainActivity, "Zasubskrybowałeś powiadomienia SMS!", Toast.LENGTH_SHORT).show()
+            } }
+            radioGroup.setOnCheckedChangeListener(object : RadioGroup.OnCheckedChangeListener{
+                override fun onCheckedChanged(
+                    group: RadioGroup,
+                    checkedId: Int
+                ) {
+                    Toast.makeText(this@MainActivity, findViewById<RadioButton>(checkedId).text, Toast.LENGTH_SHORT).show()
+                }
             })
         }
     }
